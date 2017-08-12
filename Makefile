49d2e96f454a6dd53e479a4b6aac1558e55bfb25
Victor Berger <victor.berger@m4x.org>
2017-08-12 19:33:22 +0200

    Travis deployment

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 
 install-deps:
-	npm install -g elm elm-css
+	npm install -g elm-css
 	elm package install
 
 build: main.js main.css
 
 main.js: src/*.elm
-	elm make src/Main.elm --output=main.js
+	elm make src/Main.elm --yes --output=main.js
 
 main.css: src/Style.elm
 	elm css src/Stylesheets.elm
---
+++ gh-pages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+rm -rf gh-pages || exit 0;
+
+mkdir -p gh-pages
+
+cp index.html main.css main.js gh-pages
+
+cd gh-pages
+
+# init branch and commit
+git init
+git config user.name "Victor Berger (via Travis CI)"
+git config user.email "victor.berger@m4x.org"
+git add .
+git commit -m "Deploy to GitHub Pages [Travus CI]"
+git push --force "https://${GH_TOKEN}@github.com/vberger/idfma.git" master:gh-pages
